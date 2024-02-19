0a10c846fead1b995bbaa10b4e0457dafb981bee
Siddardha Kavuri <66861559+siddardhakavuri@users.noreply.github.com>
2024-02-18 18:23:17 -0800

    Add files via upload
    Changed script to wait until claim button is clickable.

--- aws_daily.py
+++ aws_daily.py
@@ -7,43 +7,51 @@ This is a script to collect daily rewards on AWS Emerging Talent.
 This script uses Selenium to automate the process of logging in to the AWS Emerging Talent website,
 claiming the daily reward, and printing a success message.
 """
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
-PATH = "/usr/bin/chromedriver"
+def reward_collector():
+    """
+        Returns:
+        error code 1 if the script times out
+    """
+    path = "/usr/bin/chromedriver"
 
-cService = webdriver.ChromeService(executable_path=PATH)
+    cService = webdriver.ChromeService(executable_path=path)
 
-driver = webdriver.Chrome(service=cService)
+    driver = webdriver.Chrome(service=cService)
 
-driver.get("https://aws-emergingtalent.influitive.com/")
-DELAY = 30 # seconds
-try:
-    login = WebDriverWait(driver, DELAY).until(
-        EC.presence_of_element_located((By.LINK_TEXT, 'Log In'))
-    )
-    login.click()
-    username = WebDriverWait(driver, DELAY).until(
-        EC.presence_of_element_located((By.ID, 'user_email'))
-    )
-    # TODO
-    # Replace the asterisks with your email and password
-    username.send_keys("*************@gmail.com")
-    password = driver.find_element(By.ID, "user_password")
-    password.send_keys("************")
-    signin = driver.find_element(By.ID, "sign-in-button")
-    signin.click()
-except TimeoutException:
-    print("User login took too much time!")
+    driver.get("https://aws-emergingtalent.influitive.com/")
+    delay = 30 # seconds
+    try:
+        login = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.LINK_TEXT, 'Log In'))
+        )
+        login.click()
+        username = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.ID, 'user_email'))
+        )
 
-try:
-    claim = WebDriverWait(driver, DELAY).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'css-10meywq'))
-    )
-    claim.click()
-    print("Successfully claimed daily reward!")
-except TimeoutException:
-    print ("Claiming took too much time!")
+        # Replace the asterisks with your email and password
+        username.send_keys("************")
+        password = driver.find_element(By.ID, "user_password")
+        password.send_keys("************")
+        signin = driver.find_element(By.ID, "sign-in-button")
+        signin.click()
+    except TimeoutException:
+        print("User login took too much time!")
+
+    try:
+        button = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((
+            By.CSS_SELECTOR, 'css-10meywq')))
+        button.click()
+        print("Successfully claimed daily reward!")
+    except TimeoutException:
+        print ("Claiming took too much time!")
+        sys.exit(1)
+
+reward_collector()
